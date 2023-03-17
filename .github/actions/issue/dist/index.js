1764ed1c3deb60d2c2f3cf2e05131b6a125ed82c
Rob Howells <rob.howells@weareaccess.co.uk>
2023-03-17 14:46:46 +0000

    Added Octokit async function.

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -9792,26 +9792,30 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
-try {
-  const token = core.getInput('token');
-  const title = core.getInput('title');
-  const body = core.getInput('body');
-  const assignees = core.getInput('assignees');
+async function run() {
+  try {
+    const token = core.getInput('token');
+    const title = core.getInput('title');
+    const body = core.getInput('body');
+    const assignees = core.getInput('assignees');
 
-  const octokit = github.getOctokit(token);
-  const response = octokit.rest.issues.create({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    title,
-    body,
-    assignees: assignees ? assignees.split(',') : undefined
-  });
+    const octokit = github.getOctokit(token);
+    const response = await octokit.rest.issues.create({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      title,
+      body,
+      assignees: assignees ? assignees.split(',') : undefined
+    });
 
-  core.setOutput('issue', JSON.stringify(response.data));
+    core.setOutput('issue', JSON.stringify(response.data));
 
-} catch (error) {
-  core.setFailed(error);
+  } catch (error) {
+    core.setFailed(error);
+  }
 }
+
+run();
 
 })();
 
